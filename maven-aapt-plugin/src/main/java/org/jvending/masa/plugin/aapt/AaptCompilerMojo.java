fe714f3aa063c0f7ce800a4e7ed6686ff3a1e296
Shane Isbell <shane.isbell@gmail.com>
2008-06-04 03:35:04 +0000

    Added dependency resolver to aapt plugin

--- maven-aapt-plugin/src/main/java/org/jvending/masa/plugin/aapt/AaptCompilerMojo.java
+++ maven-aapt-plugin/src/main/java/org/jvending/masa/plugin/aapt/AaptCompilerMojo.java
@@ -33,6 +33,7 @@ import java.util.List;
 /**
  * @goal compile
  * @phase compile
+ * @requiresDependencyResolution compile
  * @description
  */
 public class AaptCompilerMojo extends AbstractMojo {
