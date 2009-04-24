067741765b07bcefdafea2ca2d6a8f8bba0e33a1
Hugo Josefson <hugo@josefson.org>
2009-04-24 21:08:56 +0200

    Moved Device{Puller,Pusher}Mojo to package standalonemojos.

--- maven-android-plugin/src/main/java/com/jayway/maven/plugins/android/standalonemojos/DevicePullerMojo.java
+++ maven-android-plugin/src/main/java/com/jayway/maven/plugins/android/standalonemojos/DevicePullerMojo.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jayway.maven.plugins.android.phase10preintegrationtest;
+package com.jayway.maven.plugins.android.standalonemojos;
 
 import com.jayway.maven.plugins.android.CommandExecutor;
 import com.jayway.maven.plugins.android.ExecutionException;
