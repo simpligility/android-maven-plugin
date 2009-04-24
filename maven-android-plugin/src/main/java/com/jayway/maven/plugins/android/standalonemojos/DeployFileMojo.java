6677ac56c31fa199efa9e0d1ef7d7b0f73bc5fc4
Hugo Josefson <hugo@josefson.org>
2009-04-24 21:15:00 +0200

    Added goal deploy-file.

---
+++ maven-android-plugin/src/main/java/com/jayway/maven/plugins/android/standalonemojos/DeployFileMojo.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (C) 2007-2008 JVending Masa
+ * Copyright (C) 2009 Jayway AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.jayway.maven.plugins.android.standalonemojos;
+
+import com.jayway.maven.plugins.android.AbstractAndroidMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import java.io.File;
+
+/**
+ * Deploys a separate apk file to a connected device.<br/>
+ * @goal deployFile
+ * @author hugo.josefson@jayway.com
+ */
+public class DeployFileMojo extends AbstractAndroidMojo {
+
+    /**
+     * The apk file to deploy to a connected emulator or usb device.
+     * @parameter expression="${file}"
+     * @required
+     */
+    private File file;
+
+    public void execute() throws MojoExecutionException, MojoFailureException {
+
+        if (undeployApkBeforeDeploying){
+            undeployApk(file);
+        }
+        deployApk(file);
+    }
+
+}
