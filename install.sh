7c546456ce6ff7e82bb95fbb8fbfe6e458f84039
Hugo Josefson <hugo@josefson.org>
2009-06-02 10:04:14 +0200

    Removed debug output -X from install.sh.

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-mvn -X install:install-file -Dfile=$ANDROID_SDK/android.jar -DgeneratePom=true -DgroupId=android -DartifactId=android -Dversion=1.1_r1 -Dpackaging=jar
+mvn install:install-file -Dfile=$ANDROID_SDK/android.jar -DgeneratePom=true -DgroupId=android -DartifactId=android -Dversion=1.1_r1 -Dpackaging=jar
