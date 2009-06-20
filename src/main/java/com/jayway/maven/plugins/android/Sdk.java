d0f7eda9883c526c584d057de47c7f521dc6df76
Hugo Josefson <hugo@josefson.org>
2009-06-20 20:10:05 +0200

    Split up Sdk into one class for just the config parameters (Sdk) and one for the logic (AndroidSdk).

--- src/main/java/com/jayway/maven/plugins/android/Sdk.java
+++ src/main/java/com/jayway/maven/plugins/android/Sdk.java
@@ -29,10 +29,6 @@ import java.util.Arrays;
  *
  * TODO: do more thorough parameter checking
  *
- * TODO: split this up into:
- * TODO:     one parameter java bean, and
- * TODO:     one object with functionality which reads parameters from parameter values, and then we have all this functionality there
- *
  * @author hugo.josefson@jayway.com
  */
 public class Sdk {
@@ -45,31 +41,6 @@ public class Sdk {
      */
     private File path;
 
-
-    public enum Layout{LAYOUT_1_1, LAYOUT_1_5};
-
-    public Layout getLayout() {
-
-        assertPathIsDirectory(path);
-
-        final File platforms = new File(path, "platforms");
-        if (platforms.exists() && platforms.isDirectory()){
-            return Layout.LAYOUT_1_5;
-        }
-
-        final File androidJar = new File(path, "android.jar");
-        if (androidJar.exists() && androidJar.isFile()) {
-            return Layout.LAYOUT_1_1;
-        }
-
-        throw new InvalidSdkException("Android SDK could not be identified from path \"" + path +"\"");
-    }
-
-    private void assertPathIsDirectory(final File path) {
-        if (path == null) throw new InvalidSdkException("Path is null!");
-        if (!path.isDirectory()) throw new InvalidSdkException("Path \"" + path + "\" is not a directory.");
-    }
-
     /**
      * <p>Chosen platform version. Valid values are whichever platforms are available in the SDK, under the directory
      * <code>platforms</code>. Defaults to the highest available one if not set.</p>
@@ -80,98 +51,11 @@ public class Sdk {
      */
     private String platform;
 
-    private static final Set<String> commonToolsIn11And15 = new HashSet<String>() {
-        {
-            add("adb"            );
-            add("android"        );
-            add("apkbuilder"     );
-            add("ddms"           );
-            add("dmtracedump"    );
-            add("draw9patch"     );
-            add("emulator"       );
-            add("hierarchyviewer");
-            add("hprof-conv"     );
-            add("mksdcard"       );
-            add("sqlite3"        );
-            add("traceview"      );
-        }
-    };
-
-    /**
-     * Returns the complete path for a tool, based on this SDK.
-     * @param tool which tool, for example <code>adb</code>.
-     * @return the complete path as a <code>String</code>, including the tool's filename.
-     */
-    public String getPathForTool(String tool) {
-        if (getLayout() == Layout.LAYOUT_1_1) {
-            return path + "/tools/" + tool;
-        }
-
-        if (getLayout() == Layout.LAYOUT_1_5) {
-            if (commonToolsIn11And15.contains(tool)) {
-                return path + "/tools/" + tool;
-            }else {
-
-                return getPlatform() + "/tools/" + tool;
-            }
-        }
-
-        throw new InvalidSdkException("Unsupported layout \"" + getLayout() + "\"!");
+    public File getPath() {
+        return path;
     }
 
-
-    /**
-     * Returns the complete path for <code>framework.aidl</code>, based on this SDK.
-     * @return the complete path as a <code>String</code>, including the filename.
-     */
-    public String getPathForFrameworkAidl() {
-        if (getLayout() == Layout.LAYOUT_1_1) {
-            return path + "/tools/lib/framework.aidl";
-        }
-
-        if (getLayout() == Layout.LAYOUT_1_5) {
-            return getPlatform() + "/framework.aidl";
-        }
-
-        throw new InvalidSdkException("Unsupported layout \"" + getLayout() + "\"!");
-    }
-
-    /**
-     * Resolves the android.jar from this SDK.
-     *
-     * @return a <code>File</code> pointing to the android.jar file.
-     * @throws org.apache.maven.plugin.MojoExecutionException if the file can not be resolved.
-     */
-    public File getAndroidJar() throws MojoExecutionException {
-        if (getLayout() == Layout.LAYOUT_1_1) {
-            return new File(path + "/android.jar");
-        }
-
-        if (getLayout() == Layout.LAYOUT_1_5) {
-            return new File(getPlatform() + "/android.jar");
-        }
-
-        throw new MojoExecutionException("Invalid Layout \"" + getLayout() + "\"!");
-    }
-
-    public File getPlatform() {
-        assertPathIsDirectory(path);
-
-        if (getLayout() == Layout.LAYOUT_1_1){
-            return path;
-        }
-
-        final File platformsDirectory = new File(path, "platforms");
-        assertPathIsDirectory(platformsDirectory);
-
-        if (platform == null){
-            final File[] platformDirectories = platformsDirectory.listFiles();
-            Arrays.sort(platformDirectories);
-            return platformDirectories[platformDirectories.length-1];
-        }else{
-            final File platformDirectory = new File(platformsDirectory, "android-" + platform);
-            assertPathIsDirectory(platformDirectory);
-            return platformDirectory;
-        }
+    public String getPlatform() {
+        return platform;
     }
 }
