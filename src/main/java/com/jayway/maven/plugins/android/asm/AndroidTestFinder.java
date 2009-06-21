76ec7f22a34861497c4a12a830b671eec8544091
Hugo Josefson <hugo@josefson.org>
2009-06-21 21:28:05 +0200

    Turned local test into a general test.

--- src/main/java/com/jayway/maven/plugins/android/asm/AndroidTestFinder.java
+++ src/main/java/com/jayway/maven/plugins/android/asm/AndroidTestFinder.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2009 Jayway AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jayway.maven.plugins.android.asm;
 
 import org.codehaus.plexus.util.DirectoryWalkListener;
@@ -13,6 +28,8 @@ import java.util.List;
 
 /**
  * Finds Android platformtest classes in a directory of compiled Java classes.
+ *
+ * @author hugo.josefson@jayway.com
  */
 public class AndroidTestFinder {
 
--- src/test/java/com/jayway/maven/plugins/android/asm/AndroidTestFinderTest.java
+++ src/test/java/com/jayway/maven/plugins/android/asm/AndroidTestFinderTest.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2009 Jayway AB
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.jayway.maven.plugins.android.asm;
 
 import org.junit.Test;
@@ -13,13 +28,14 @@ import java.io.File;
  */
 public class AndroidTestFinderTest {
     @Test
-    public void givenLocalAndroidAppDirectoryThenNoTests() throws MojoExecutionException {
-        final boolean result = AndroidTestFinder.containsAndroidTests(new File("/home/hugo/code/way/maven-android-plugin-samples/apidemos-15/apidemos-15-app/target/android-classes"));
-        Assert.assertFalse("apidemos-15-app should not contain any tests.", result);
+    public void givenDirectoryWithoutTestsThenNoTests() throws MojoExecutionException {
+        final boolean result = AndroidTestFinder.containsAndroidTests(new File("target/test-classes/com/jayway/maven/plugins/android/asm/withouttests"));
+        Assert.assertFalse("'withouttests' should not contain any tests.", result);
     }
     @Test
-    public void givenLocalPlatformTestDirectoryThenItContainsTests() throws MojoExecutionException {
-        final boolean result = AndroidTestFinder.containsAndroidTests(new File("/home/hugo/code/way/maven-android-plugin-samples/apidemos-15/apidemos-15-platformtests/target/android-classes"));
-        Assert.assertTrue("apidemos-15-platformtests should contain tests.", result);
+    public void givenDirectoryWithTestsThenItContainsTests() throws MojoExecutionException {
+        final boolean result = AndroidTestFinder.containsAndroidTests(new File("target/test-classes/com/jayway/maven/plugins/android/asm/withtests"));
+        Assert.assertTrue("'withtests' should contain tests.", result);
     }
+
 }
