afb7948ff28f07e01891299c10157f3390572d1b
Manfred Moser (mosabua) <manfred@mosabuam.com>
2010-02-10 13:58:55 -0800

    refactored to separate stop and start emulator mojos, changed so that commandline and pom parameters work as well as changed parameter names, stop mojo not yet implmeneted but the shell is there

---
+++ src/main/java/com/jayway/maven/plugins/android/standalonemojos/EmulatorStartMojo.java
@@ -0,0 +1,31 @@
+package com.jayway.maven.plugins.android.standalonemojos;
+
+import com.jayway.maven.plugins.android.AbstractAndroidMojo;
+import com.jayway.maven.plugins.android.CommandExecutor;
+import com.jayway.maven.plugins.android.ExecutionException;
+import org.apache.commons.lang.StringUtils;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * EmulatorStartMojo can start the Android Emulator with a specified Android Virtual Device (avd).
+ * 
+ * @author Manfred Moser <manfred@simpligility.com>
+ * @goal emulator-start
+ * @requiresProject false
+ */
+public class EmulatorStartMojo extends AbstractAndroidMojo {
+
+    /**
+     * Start the Android Emulator.
+     * @throws MojoExecutionException
+     * @throws MojoFailureException
+     */
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        startAndroidEmulator();
+    }
+
+}
