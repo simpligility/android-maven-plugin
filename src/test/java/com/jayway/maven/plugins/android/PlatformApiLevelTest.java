8529e019601adc15e3002bdb8345041dae9125bc
Manfred Moser (mosabua) <manfred@mosabuam.com>
2010-05-06 23:15:48 -0700

    introduced better error handling for sdk platform as well as allowed for usage of platform as well as api level as parameter with internal automatic translation

---
+++ src/test/java/com/jayway/maven/plugins/android/PlatformApiLevelTest.java
@@ -0,0 +1,64 @@
+package com.jayway.maven.plugins.android;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ *
+ * @author Manfred Moser <manfred@simpligility.com>
+ */
+public class PlatformApiLevelTest {
+
+    @Test
+    public void checkTotalNumber() {
+        Assert.assertEquals(6, PlatformApiLevel.values().length);
+    }
+
+    @Test
+    public void checkApiLevelLookup() {
+        Assert.assertEquals("1.1", PlatformApiLevel.findPlatform("2"));
+        Assert.assertEquals("1.5", PlatformApiLevel.findPlatform("3"));
+        Assert.assertEquals("1.6", PlatformApiLevel.findPlatform("4"));
+        Assert.assertEquals("2.0", PlatformApiLevel.findPlatform("5"));
+        Assert.assertEquals("2.01", PlatformApiLevel.findPlatform("6"));
+        Assert.assertEquals("2.1", PlatformApiLevel.findPlatform("7"));
+    }
+
+    @Test
+    public void checkPlatformLookup() {
+        Assert.assertEquals("2", PlatformApiLevel.findApiLevel("1.1"));
+        Assert.assertEquals("3", PlatformApiLevel.findApiLevel("1.5"));
+        Assert.assertEquals("4", PlatformApiLevel.findApiLevel("1.6"));
+        Assert.assertEquals("5", PlatformApiLevel.findApiLevel("2.0"));
+        Assert.assertEquals("6", PlatformApiLevel.findApiLevel("2.01"));
+        Assert.assertEquals("7", PlatformApiLevel.findApiLevel("2.1"));
+    }
+
+    @Test
+    public void checkValidPlatform() {
+        Assert.assertTrue(PlatformApiLevel.isValidApiLevel("2"));
+        Assert.assertTrue(PlatformApiLevel.isValidApiLevel("3"));
+        Assert.assertTrue(PlatformApiLevel.isValidApiLevel("4"));
+        Assert.assertTrue(PlatformApiLevel.isValidApiLevel("5"));
+        Assert.assertTrue(PlatformApiLevel.isValidApiLevel("6"));
+        Assert.assertTrue(PlatformApiLevel.isValidApiLevel("7"));
+
+        Assert.assertFalse(PlatformApiLevel.isValidApiLevel("something"));
+        Assert.assertFalse(PlatformApiLevel.isValidApiLevel("1.0"));
+    }
+
+    @Test
+    public void checkValidApiLevel() {
+
+        Assert.assertTrue(PlatformApiLevel.isValidPlatform("1.1"));
+        Assert.assertTrue(PlatformApiLevel.isValidPlatform("1.5"));
+        Assert.assertTrue(PlatformApiLevel.isValidPlatform("1.6"));
+        Assert.assertTrue(PlatformApiLevel.isValidPlatform("2.0"));
+        Assert.assertTrue(PlatformApiLevel.isValidPlatform("2.01"));
+        Assert.assertTrue(PlatformApiLevel.isValidPlatform("2.1"));
+
+        Assert.assertFalse(PlatformApiLevel.isValidPlatform("something"));
+        Assert.assertFalse(PlatformApiLevel.isValidPlatform("1.0"));
+    }
+}
