557344d3a3ae5237cfce3a2d84a51ea99d493393
Hugo Josefson <hugo@josefson.org>
2010-09-25 16:06:16 +0200

    Renamed packages to make room for phase08-preparepackage

--- src/main/java/com/jayway/maven/plugins/android/phase12integrationtest/InternalIntegrationTestMojo.java
+++ src/main/java/com/jayway/maven/plugins/android/phase12integrationtest/InternalIntegrationTestMojo.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.jayway.maven.plugins.android.phase11integrationtest;
+package com.jayway.maven.plugins.android.phase12integrationtest;
 
 import com.jayway.maven.plugins.android.standalonemojos.AbstractInstrumentationMojo;
 import org.apache.maven.plugin.MojoExecutionException;
