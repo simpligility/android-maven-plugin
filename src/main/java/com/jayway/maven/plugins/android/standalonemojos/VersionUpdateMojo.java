4d6c434a4b07fa1f9b7fbf4badb655c9aa828f11
Joakim Erdfelt <joakim@erdfelt.com>
2011-02-09 16:59:15 -0700

    Adding auto-increment of android:versionCode as configurable option

--- src/main/java/com/jayway/maven/plugins/android/standalonemojos/VersionUpdateMojo.java
+++ src/main/java/com/jayway/maven/plugins/android/standalonemojos/VersionUpdateMojo.java
@@ -18,6 +18,7 @@ import javax.xml.transform.stream.StreamResult;
 
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang.math.NumberUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.w3c.dom.Attr;
@@ -28,10 +29,55 @@ import org.xml.sax.SAXException;
 import com.jayway.maven.plugins.android.AbstractAndroidMojo;
 
 /**
- * Writes the project version into <code>android:versionName</code> in the <code>AndroidManifest.xml</code> on
- * projects that have <code>&lt;packaging&gt;apk&lt;/packaging&gt;</code>
+ * Updates various version attributes present in the <code>AndroidManifest.xml</code> file.
+ * <p>
+ * You can configure this mojo to update the <code>android:versionName</code> attribute 
+ * automatically to the value present in your project's version.
+ * <p>
+ * You can also configure this mojo to auto-increment the <code>android:versionCode</code>
+ * attribute on each build as well.
  * <p>
  * Note: This process might reformat the <code>AndroidManifest.xml</code> per JAXP {@link Transformer} defaults.
+ * <p>
+ * Updating Your <code>android:versionName</code> attribute
+ * <pre>
+ *   &lt;plugin&gt;
+ *     &lt;groupId&gt;com.jayway.maven.plugins.android.generation2&lt;/groupId&gt;
+ *     &lt;artifactId&gt;maven-android-plugin&lt;/artifactId&gt;
+ *     &lt;executions&gt;
+ *       &lt;execution&gt;
+ *         &lt;id&gt;update-version&lt;/id&gt;
+ *         &lt;goals&gt;
+ *           &lt;goal&gt;version-update&lt;/goal&gt;
+ *         &lt;/goals&gt;
+ *         &lt;configuration&gt;
+ *           &lt;versionNameUpdate&gt;true&lt;/versionNameUpdate&gt;
+ *         &lt;/configuration&gt;
+ *       &lt;/execution&gt;
+ *     &lt;/executions&gt;
+ *   &lt;/plugin&gt;
+ * </pre>
+ * <p>
+ * Auto-Incrementing your <code>android:versionCode</code> attribute
+ * <pre>
+ *   &lt;plugin&gt;
+ *     &lt;groupId&gt;com.jayway.maven.plugins.android.generation2&lt;/groupId&gt;
+ *     &lt;artifactId&gt;maven-android-plugin&lt;/artifactId&gt;
+ *     &lt;executions&gt;
+ *       &lt;execution&gt;
+ *         &lt;id&gt;update-version&lt;/id&gt;
+ *         &lt;goals&gt;
+ *           &lt;goal&gt;version-update&lt;/goal&gt;
+ *         &lt;/goals&gt;
+ *         &lt;configuration&gt;
+ *           &lt;versionCodeAutoIncrement&gt;true&lt;/versionCodeAutoIncrement&gt;
+ *         &lt;/configuration&gt;
+ *       &lt;/execution&gt;
+ *     &lt;/executions&gt;
+ *   &lt;/plugin&gt;
+ * </pre>
+ *
+ * 
  * 
  * @author joakim@erdfelt.com
  * @goal version-update
@@ -39,23 +85,38 @@ import com.jayway.maven.plugins.android.AbstractAndroidMojo;
  * @phase prepare-resources
  */
 public class VersionUpdateMojo extends AbstractAndroidMojo {
+    private static final String ATTR_VERSION_NAME        = "android:versionName";
+    private static final String ATTR_VERSION_CODE        = "android:versionCode";
+
+    /**
+     * Update the <code>android:versionName</code> with the <code>&lt;version&gt;</code> value from the maven project.
+     * 
+     * @parameter expression="${android.versionname.update}" default-value="true"
+     */
+    private boolean             versionNameUpdate        = true;
+
+    /**
+     * Auto increment the <code>android:versionCode</code> attribute with each build.
+     * 
+     * @parameter expression="${android.versioncode.autoincrement}" default-value="false"
+     */
+    private boolean             versionCodeAutoIncrement = false;
 
     public void execute() throws MojoExecutionException, MojoFailureException {
         if (!"apk".equals(project.getPackaging())) {
             return; // skip, not an android project.
         }
-        
-        if( androidManifestFile == null ) {
+
+        if (androidManifestFile == null) {
             return; // skip, no androidmanifest.xml defined (rare case)
         }
-        
+
         if (!androidManifestFile.exists()) {
             return; // skip, no AndroidManifest.xml file found.
         }
 
-        String version = project.getVersion();
         try {
-            updateManifest(androidManifestFile, version);
+            updateManifest(androidManifestFile);
         } catch (IOException e) {
             throw new MojoFailureException("XML I/O error: " + androidManifestFile, e);
         } catch (ParserConfigurationException e) {
@@ -100,15 +161,36 @@ public class VersionUpdateMojo extends AbstractAndroidMojo {
         }
     }
 
-    public void updateManifest(File manifestFile, String version) throws IOException, ParserConfigurationException,
-            SAXException, TransformerException {
+    public void updateManifest(File manifestFile) throws IOException, ParserConfigurationException, SAXException,
+            TransformerException {
         Document doc = readManifest(manifestFile);
 
         Element root = doc.getDocumentElement();
-        Attr versionName = root.getAttributeNode("android:versionName");
 
-        if ((versionName == null) || (StringUtils.equals(version, versionName.getValue()) == false)) {
-            root.setAttribute("android:versionName", version);
+        boolean dirty = false;
+
+        if (versionNameUpdate) {
+            String projectVersion = project.getVersion();
+            Attr versionName = root.getAttributeNode(ATTR_VERSION_NAME);
+
+            if ((versionName == null) || (StringUtils.equals(projectVersion, versionName.getValue()) == false)) {
+                root.setAttribute(ATTR_VERSION_NAME, projectVersion);
+                dirty = true;
+            }
+        }
+
+        if (versionCodeAutoIncrement) {
+            Attr versionCode = root.getAttributeNode(ATTR_VERSION_CODE);
+            int currentVersionCode = 0;
+            if (versionCode != null) {
+                currentVersionCode = NumberUtils.toInt(versionCode.getValue(), 0);
+            }
+            currentVersionCode++;
+            root.setAttribute(ATTR_VERSION_CODE, String.valueOf(currentVersionCode));
+            dirty = true;
+        }
+
+        if (dirty) {
             writeManifest(manifestFile, doc);
         }
     }
