fb873bd331b48802b9d781ccb9cce064da4bc77d
Manfred Moser (mosabua) <manfred@mosabuam.com>
2011-05-30 22:09:45 -0700

    Removed automatic scope provided setting for android groupid (breaks backwards compatibilty)

--- src/main/java/com/jayway/maven/plugins/android/AbstractAndroidMojo.java
+++ src/main/java/com/jayway/maven/plugins/android/AbstractAndroidMojo.java
@@ -47,6 +47,7 @@ import static org.apache.commons.lang.StringUtils.isBlank;
  * Contains common fields and methods for android mojos.
  *
  * @author hugo.josefson@jayway.com
+ * @author Manfred Moser <manfred@simpligility.com>>
  */
 public abstract class AbstractAndroidMojo extends AbstractMojo {
 
@@ -73,6 +74,7 @@ public abstract class AbstractAndroidMojo extends AbstractMojo {
      * @readonly
      */
     protected MavenSession session;
+
 
     /**
      * The java sources directory.
@@ -471,13 +473,7 @@ public abstract class AbstractAndroidMojo extends AbstractMojo {
                 continue;
             }
 
-            // TODO: this statement must be retired in version 3.0, but we can't do that yet because we promised to not break backwards compatibility within the 2.x series.
-            if (artifact.getGroupId().equals("android")) {
-                getLog().warn("Excluding the android.jar from being unpacked into your apk file, based on its <groupId>android</groupId>. Please set <scope>provided</scope> in that dependency, because that is the correct way, and the only which will work in the future.");
-                continue;
-            }
-
-            results.add(artifact);
+           results.add(artifact);
         }
         return results;
     }
