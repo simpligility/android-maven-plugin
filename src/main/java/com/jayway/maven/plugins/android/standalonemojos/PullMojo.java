4c8271106fe65b87a30364e65d49c1d8d79f38ee
Nic Strong <nic.strong@gmail.com>
2011-08-24 18:17:06 +1200

    Added support to the pull mojo for creating the destination directory if it does not already exist

--- src/main/java/com/jayway/maven/plugins/android/standalonemojos/PullMojo.java
+++ src/main/java/com/jayway/maven/plugins/android/standalonemojos/PullMojo.java
@@ -25,11 +25,13 @@ import com.android.ddmlib.SyncException;
 import com.android.ddmlib.SyncService;
 import com.android.ddmlib.TimeoutException;
 import com.jayway.maven.plugins.android.common.LogSyncProgressMonitor;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 
 import com.jayway.maven.plugins.android.AbstractAndroidMojo;
 import com.jayway.maven.plugins.android.DeviceCallback;
+import sun.awt.image.OffScreenImage;
 
 /**
  * Copy file from all the attached (or specified) devices/emulators.
@@ -58,6 +60,20 @@ public class PullMojo extends AbstractAndroidMojo {
      */
     private File destination;
 
+	/**
+	 * Create destination directory if it doesn't exist (using
+	 * File.mkdirs()). Because we don't know whether the specified
+	 * destination is a directory or file (unless it exists in
+	 * which case this flag is ignored) we assume that unless
+	 * the destination explicity ends with / it will be treated
+	 * as a file (and removed to get the destination directory)
+	 *
+	 * @parameter default-value=false expression="${android.pull.createDestDir}"
+	 */
+	private Boolean createDestDir;
+
+
+
     public void execute() throws MojoExecutionException, MojoFailureException {
         final String sourcePath = source;
         final String destinationPath;
@@ -68,6 +84,16 @@ public class PullMojo extends AbstractAndroidMojo {
         } else {
             destinationPath = destination.getAbsolutePath();
         }
+
+	    if (createDestDir && !destination.exists()) {
+		    String destPath = destination.getAbsolutePath();
+		    destPath = FilenameUtils.getFullPath(destPath);
+		    File destFile = new File(destPath);
+		    if (!destFile.exists()) {
+				getLog().info("Creating destination directory " + destFile);
+			    destFile.mkdirs();
+		    }
+	    }
 
         final String message = "Pull of " + source + " to " + destinationPath +
             " from ";
