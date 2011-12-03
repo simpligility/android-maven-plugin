dc63186672f9ee22920760a8a1f8888bca98f904
Johan Lindquist <johan@loki.(none)>
2011-12-02 23:47:10 -0500

    now cleans basedir/obj and basedir/libs - optional to skip

--- src/main/java/com/jayway/maven/plugins/android/phase00clean/NdkCleanMojo.java
+++ src/main/java/com/jayway/maven/plugins/android/phase00clean/NdkCleanMojo.java
@@ -3,6 +3,11 @@ package com.jayway.maven.plugins.android.phase00clean;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.IOUtil;
+
+import java.io.File;
+import java.io.IOException;
 
 /**
  * @author Johan Lindquist <johanlindquist@gmail.com>
@@ -13,9 +18,46 @@ import org.apache.maven.plugin.MojoFailureException;
  */
 public class NdkCleanMojo extends AbstractMojo {
 
+    /**
+     * @parameter expression="${android.nativeBuildLibsOutputDirectory}" default-value="${project.basedir}/libs"
+     */
+    File ndkBuildLibsOutputDirectory;
+
+    /**
+     * @parameter expression="${android.nativeBuildObjOutputDirectory}" default-value="${project.basedir}/obj"
+     */
+    File ndkBuildObjOutputDirectory;
+
+    /** Forces the clean process to be skipped.
+     *
+     * @parameter expression="${android.nativeBuildSkipClean}" default-value="false"
+     */
+    boolean skipClean = false;
+
     @Override
     public void execute() throws MojoExecutionException, MojoFailureException {
-        getLog().info("Cleaning out native code");
+        getLog().debug("Cleaning out native library code directory : " + ndkBuildLibsOutputDirectory.getAbsolutePath());
+
+        if (ndkBuildLibsOutputDirectory.exists())
+        {
+            try {
+                FileUtils.deleteDirectory( ndkBuildLibsOutputDirectory );
+            } catch ( IOException e ) {
+                getLog().error( "Error deleting directory: " + e.getMessage(), e);
+            }
+        }
+
+        getLog().debug("Cleaning out native object code directory: " + ndkBuildObjOutputDirectory.getAbsolutePath());
+
+        if (ndkBuildObjOutputDirectory.exists())
+        {
+            try {
+                FileUtils.deleteDirectory( ndkBuildObjOutputDirectory );
+            } catch ( IOException e ) {
+                getLog().error( "Error deleting directory: " + e.getMessage(), e);
+            }
+        }
+
     }
 
 }
