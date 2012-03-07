dd740faf213e083a7698443df8985a758a448f36
Adrian Stabiszewski <as@grundid.de>
2012-03-07 15:45:22 +0100

    annotated config

---
+++ src/main/java/com/jayway/maven/plugins/android/config/PullParameter.java
@@ -0,0 +1,16 @@
+package com.jayway.maven.plugins.android.config;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface PullParameter {
+
+	String defaultValue() default "";
+	
+	String defaultValueGetterMethod() default "";
+}
