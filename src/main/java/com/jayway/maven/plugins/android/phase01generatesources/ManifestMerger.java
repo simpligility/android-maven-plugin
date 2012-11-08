750bcc6bf0c88f148cad47543c47e0beafe5943b
Manfred Moser <manfred@simpligility.com>
2012-11-08 09:26:11 -0800

    undo code reformatting from pull request from Tom

--- src/main/java/com/jayway/maven/plugins/android/phase01generatesources/ManifestMerger.java
+++ src/main/java/com/jayway/maven/plugins/android/phase01generatesources/ManifestMerger.java
@@ -35,16 +35,14 @@ public class ManifestMerger
     /**
      * Before being able to use the ManifestMerger, an initialization is required.
      * 
-     * @param log
-     *            the Mojo Logger
-     * @param sdkLibs
-     *            the File pointing on {@code sdklib.jar}
-     * @param mergerLib
-     *            the File pointing on {@code manifmerger.jar}
-     * @throws MojoExecutionException
-     *             if the ManifestMerger class cannot be loaded
+     * @param log the Mojo Logger
+     * @param sdkLibs the File pointing on {@code sdklib.jar}
+     * @param mergerLib the File pointing on {@code manifmerger.jar}
+     * @throws MojoExecutionException if the ManifestMerger class cannot be loaded
      */
-    @SuppressWarnings( { "unchecked", "rawtypes" } )
+    @SuppressWarnings( {
+    "unchecked", "rawtypes"
+    } )
     public void initialize( Log log, File sdkLibs, File mergerLib ) throws MojoExecutionException
     {
         if ( processMethod != null && merger != null )
@@ -61,8 +59,9 @@ public class ManifestMerger
         Class mergerLogClass = null;
         try
         {
-            mlLoader = new URLClassLoader( new URL[] { mergerLib.toURI().toURL() },
-                    ManifestMerger.class.getClassLoader() );
+            mlLoader = new URLClassLoader( new URL[] {
+                mergerLib.toURI().toURL()
+            }, ManifestMerger.class.getClassLoader() );
             manifestMergerClass = mlLoader.loadClass( "com.android.manifmerger.ManifestMerger" );
             log.debug( "ManifestMerger loaded " + manifestMergerClass );
             mergerLogClass = mlLoader.loadClass( "com.android.manifmerger.MergerLog" );
@@ -141,15 +140,11 @@ public class ManifestMerger
     /**
      * Merge the AndroidManifests
      * 
-     * @param paramFile1
-     *            The destination File for the merged content
-     * @param paramFile2
-     *            The original AndroidManifest to merge into
-     * @param paramArayOfFile
-     *            The array of APKLIB manifests to merge
+     * @param paramFile1 The destination File for the merged content
+     * @param paramFile2 The original AndroidManifest to merge into
+     * @param paramArayOfFile The array of APKLIB manifests to merge
      * @return
-     * @throws MojoExecutionException
-     *             if there is a problem merging
+     * @throws MojoExecutionException if there is a problem merging
      */
     public boolean process( File paramFile1, File paramFile2, File[] paramArayOfFile ) throws MojoExecutionException
     {
