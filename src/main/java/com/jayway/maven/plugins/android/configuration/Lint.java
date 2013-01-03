dbdd226c49475dcfdae89bf9c0e511d316836748
Manfred Moser <manfred@simpligility.com>
2013-01-03 09:17:47 -0800

    removed redundant javadoc and cleaned up prior to renaming parameters

--- src/main/java/com/jayway/maven/plugins/android/configuration/Lint.java
+++ src/main/java/com/jayway/maven/plugins/android/configuration/Lint.java
@@ -5,97 +5,38 @@ package com.jayway.maven.plugins.android.configuration;
  * {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo} and used there.
  * 
  * @author Stéphane Nicolas <snicolas@octo.com>
+ * @author Manfred Moser <manfred@simpligility.com>
+ * @see com.jayway.maven.plugins.android.standalonemojos.LintMojo
  */
 public class Lint
 {
-
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintFailOnError}
-     */
     private Boolean failOnError;
-
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintSkip}
-     */
     private Boolean skip;
 
     // ---------------
     // Enabled Checks
     // ---------------
-
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintNoWarn}
-     */
     private Boolean nowarn;
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintWall}
-     */
     private Boolean wall;
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintWerror}
-     */
     private Boolean werror;
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintConfig}
-     */
     private String config;
 
     // ---------------
     // Output Options
     // ---------------
-
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintFullpath}
-     */
     private Boolean fullpath;
-
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintShowall}
-     */
     private Boolean showall;
-
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintNolines}
-     */
     private Boolean nolines;
-
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintUrl}
-     */
     private Boolean url;
-
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintHtml}
-     */
     private String html;
-
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintSimplehtml}
-     */
     private String simplehtml;
-
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintXml}
-     */
     private String xml;
 
     // ---------------
     // Project Options
     // ---------------
-
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintSources}
-     */
     private String sources;
-
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintClasspath}
-     */
     private String classpath;
-
-    /**
-     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.LintMojo#lintLibraries}
-     */
     private String libraries;
 
     // ---------------
@@ -171,5 +112,4 @@ public class Lint
     {
         return libraries;
     }
-
 }
