2bb13083333c01cda78e36948f850274e76a9664
Frank Bregulla <frank.bregulla@elitepartner.de>
2013-01-29 14:53:13 +0100

    Fixed issue 270: enhanced emulator-start, so that maven only waits until emulator is actually started and online or a timeout occurs.

--- src/test/java/com/jayway/maven/plugins/android/AbstractEmulatorMojoTest.java
+++ src/test/java/com/jayway/maven/plugins/android/AbstractEmulatorMojoTest.java
@@ -1,35 +1,135 @@
 package com.jayway.maven.plugins.android;
 
+import com.android.ddmlib.AndroidDebugBridge;
+import com.android.ddmlib.IDevice;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
-public class AbstractEmulatorMojoTest {
+import java.util.List;
 
-	private AbstractEmulatorMojo mockMojo;
+import static org.easymock.EasyMock.*;
+import static org.easymock.EasyMock.createNiceMock;
+import static org.easymock.EasyMock.verify;
+import static org.junit.Assert.fail;
+import static org.powermock.api.easymock.PowerMock.createMock;
+import static org.powermock.api.easymock.PowerMock.*;
+import static org.powermock.api.easymock.PowerMock.replay;
 
-	@Before
-	public void setUp() throws Exception {
-		mockMojo = new mockAbstractEmulatorMojo();
-	}
-	
-	@Ignore
-	@Test
-	public final void testStartAndStopAndroidEmulator() throws MojoExecutionException {
-		mockMojo.startAndroidEmulator();
-		mockMojo.stopAndroidEmulator();
-	}
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({ AndroidDebugBridge.class, CommandExecutor.Factory.class })
+public class AbstractEmulatorMojoTest
+{
 
-	private class mockAbstractEmulatorMojo extends AbstractEmulatorMojo {
+    private static final String AVD_NAME = "emulator";
+    private static final int WAIT = 500;
+    private AbstractEmulatorMojo abstractEmulatorMojo;
+    private CommandExecutor mockExecutor;
+    private AndroidDebugBridge mockAndroidDebugBridge;
 
-		public void execute() throws MojoExecutionException,
-		MojoFailureException {
-			// TODO Auto-generated method stub
+    @Before
+    public void setUp() throws Exception
+    {
+        mockExecutor = createNiceMock( CommandExecutor.class );
+        mockExecutor.executeCommand( anyObject( String.class ), isNull( List.class ) );
+        replay( mockExecutor );
 
-		}
+        mockStatic( CommandExecutor.Factory.class );
+        expect( CommandExecutor.Factory.createDefaultCommmandExecutor() ).andReturn( mockExecutor );
+        replay( CommandExecutor.Factory.class );
 
-	}
+        mockAndroidDebugBridge = createMock( AndroidDebugBridge.class );
+
+        abstractEmulatorMojo = new AbstractEmulatorMojoToTest();
+    }
+
+    @Test
+    public void testStartAndroidEmulatorSuccessfully() throws MojoExecutionException, ExecutionException
+    {
+        boolean onlineAtSecondTry = true;
+        IDevice emulatorDevice = withEmulatorDevice( onlineAtSecondTry );
+        withConnectedDebugBridge( emulatorDevice );
+
+        abstractEmulatorMojo.startAndroidEmulator();
+
+        verify( mockExecutor );
+    }
+
+    @Test
+    public void testStartAndroidEmulatorWithTimeoutToConnect() throws MojoExecutionException, ExecutionException
+    {
+        boolean onlineAtSecondTry = false;
+        IDevice emulatorDevice = withEmulatorDevice( onlineAtSecondTry );
+        withConnectedDebugBridge( emulatorDevice );
+
+        try
+        {
+            abstractEmulatorMojo.startAndroidEmulator();
+            fail();
+        }
+        catch ( MojoExecutionException e )
+        {
+            verify( mockExecutor );
+        }
+
+    }
+
+    private IDevice withEmulatorDevice( boolean onlineAtSecondTry )
+    {
+        IDevice emulatorDevice = createMock( IDevice.class );
+        expect( emulatorDevice.getAvdName() ).andReturn( AVD_NAME ).atLeastOnce();
+        expect( emulatorDevice.isEmulator() ).andReturn( true ).atLeastOnce();
+        if ( onlineAtSecondTry )
+        {
+            expect( emulatorDevice.isOnline() ).andReturn( false ).andReturn( true );
+        }
+        else
+        {
+            expect( emulatorDevice.isOnline() ).andReturn( false ).atLeastOnce();
+        }
+        replay( emulatorDevice );
+        return emulatorDevice;
+    }
+
+    private void withConnectedDebugBridge( IDevice emulatorDevice )
+    {
+        expect( mockAndroidDebugBridge.isConnected() ).andReturn( true );
+        expect( mockAndroidDebugBridge.hasInitialDeviceList() ).andReturn( true );
+        expect( mockAndroidDebugBridge.getDevices() ).andReturn( new IDevice[ 0 ] ).andReturn( new
+                IDevice[]{ emulatorDevice } ).atLeastOnce();
+        replay( mockAndroidDebugBridge );
+    }
+
+
+    private class AbstractEmulatorMojoToTest extends AbstractEmulatorMojo
+    {
+
+        @Override
+        public void execute() throws MojoExecutionException, MojoFailureException
+        {
+        }
+
+        @Override
+        protected AndroidDebugBridge initAndroidDebugBridge() throws MojoExecutionException
+        {
+            return mockAndroidDebugBridge;
+        }
+
+        @Override
+        String determineAvd()
+        {
+            return AVD_NAME;
+        }
+
+        @Override
+        String determineWait()
+        {
+            return String.valueOf( WAIT );
+        }
+    }
 
 }
