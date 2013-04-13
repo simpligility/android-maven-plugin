64974ec257433dd328fd1e7c45b10750e04e5438
Stéphane Nicolas <steff.nicolas@gmail.com>
2013-04-13 09:53:02 +0200

    Test MonkeyRunner parameters parsing

--- src/main/java/com/jayway/maven/plugins/android/configuration/Program.java
+++ src/main/java/com/jayway/maven/plugins/android/configuration/Program.java
@@ -9,6 +9,26 @@ package com.jayway.maven.plugins.android.configuration;
 public class Program
 {
     /**
+     * Constructor used for parsing.
+     */
+    public Program()
+    {
+        // do not remove, used by parsing.
+    }
+
+    /**
+     * Constructor used for testing.
+     * 
+     * @param filename
+     * @param options
+     */
+    public Program( String filename, String options )
+    {
+        this.filename = filename;
+        this.options = options;
+    }
+
+    /**
      * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.MonkeyRunner#filename}
      */
     private String filename;
@@ -26,4 +46,59 @@ public class Program
     {
         return options;
     }
+
+    // ----------------------------------
+    // TESTING METHODS
+    // ----------------------------------
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ( filename == null ? 0 : filename.hashCode() );
+        result = prime * result + ( options == null ? 0 : options.hashCode() );
+        return result;
+    }
+
+    @Override
+    public boolean equals( Object obj )
+    {
+        if ( this == obj )
+        {
+            return true;
+        }
+        if ( obj == null )
+        {
+            return false;
+        }
+        if ( getClass() != obj.getClass() )
+        {
+            return false;
+        }
+        Program other = (Program) obj;
+        if ( filename == null )
+        {
+            if ( other.filename != null )
+            {
+                return false;
+            }
+        }
+        else if ( !filename.equals( other.filename ) )
+        {
+            return false;
+        }
+        if ( options == null )
+        {
+            if ( other.options != null )
+            {
+                return false;
+            }
+        }
+        else if ( !options.equals( other.options ) )
+        {
+            return false;
+        }
+        return true;
+    }
+
 }
