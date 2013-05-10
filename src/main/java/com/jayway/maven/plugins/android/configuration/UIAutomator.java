35cfba2a26ff89e0ed00aaa59ae0d750605c5b32
Manfred Moser <manfred@simpligility.com>
2013-05-10 12:21:11 -0700

    code style changes to fix build

--- src/main/java/com/jayway/maven/plugins/android/configuration/UIAutomator.java
+++ src/main/java/com/jayway/maven/plugins/android/configuration/UIAutomator.java
@@ -40,10 +40,10 @@ public class UIAutomator
      * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.UIAutomatorMojo#createReport}
      */
     private Boolean createReport;
-	/**
-	 * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.UIAutomatorMojo#reportSuffix}
-	 */
-	private String reportSuffix;
+    /**
+     * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.UIAutomatorMojo#reportSuffix}
+     */
+    private String reportSuffix;
     /**
      * Mirror of {@link com.jayway.maven.plugins.android.standalonemojos.UIAutomatorMojo#takeScreenshotOnFailure}
      */
@@ -93,10 +93,10 @@ public class UIAutomator
         return createReport;
     }
 
-	public String getReportSuffix()
-	{
-		return reportSuffix;
-	}
+    public String getReportSuffix()
+    {
+        return reportSuffix;
+    }
 
     public Boolean isTakeScreenshotOnFailure()
     {
