7850a6a0aaa2d341f50c25b96c2cb19cbb1510c6
Pappy STĂNESCU <pappy.stanescu@gmail.com>
2013-11-17 17:58:23 +0200

    issue-174: added tests for UnpackMojo

---
+++ src/test/java/com/jayway/maven/plugins/android/standalonemojos/UnpackMojoTest.java
@@ -0,0 +1,94 @@
+
+package com.jayway.maven.plugins.android.standalonemojos;
+
+import org.junit.After;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import com.jayway.maven.plugins.android.AbstractAndroidMojoTestCase;
+import com.jayway.maven.plugins.android.config.ConfigHandler;
+import com.jayway.maven.plugins.android.configuration.MetaInf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RunWith( Parameterized.class )
+@FixMethodOrder( MethodSorters.NAME_ASCENDING )
+public class UnpackMojoTest
+extends AbstractAndroidMojoTestCase<UnpackMojo>
+{
+
+	@Parameters
+	static public List<Object[]> suite()
+	{
+		final List<Object[]> suite = new ArrayList<Object[]>();
+
+		suite.add( new Object[] { "unpack-config-project1", null } );
+		suite.add( new Object[] { "unpack-config-project2", new MetaInf().include( "persistence.xml" ) } );
+		suite.add( new Object[] { "unpack-config-project3", new MetaInf().include( "services/**", "persistence.xml" ) } );
+		suite.add( new Object[] { "unpack-config-project4", new MetaInf() } );
+
+		return suite;
+	}
+
+	private final String	projectName;
+
+	private final MetaInf	expected;
+
+	public UnpackMojoTest( String projectName, MetaInf expected )
+	{
+		this.projectName = projectName;
+		this.expected = expected;
+	}
+
+	@Override
+	public String getPluginGoalName()
+	{
+		return "unpack";
+	}
+
+	@Override
+	@Before
+	public void setUp()
+	throws Exception
+	{
+		super.setUp();
+	}
+
+	@Override
+	@After
+	public void tearDown()
+	throws Exception
+	{
+		super.tearDown();
+	}
+
+	@Test
+	public void testConfigHelper()
+	throws Exception
+	{
+		final UnpackMojo mojo = createMojo( this.projectName );
+		final ConfigHandler cfh = new ConfigHandler( mojo );
+
+		cfh.parseConfiguration();
+
+		MetaInf result = getFieldValue( mojo, "unpackMetaInf" );
+
+		assertEquals( this.expected, result );
+	}
+
+	protected <T> T getFieldValue( Object object, String fieldName )
+	throws IllegalAccessException
+	{
+		return (T) super.getVariableValueFromObject( object, fieldName );
+	}
+
+}
