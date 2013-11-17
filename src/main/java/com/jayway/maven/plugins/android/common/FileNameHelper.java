e3027e10100392b560c28943b9328af773b03e25
Pappy STĂNESCU <pappy.stanescu@gmail.com>
2013-11-17 19:33:18 +0200

    issue-174: restored from master

--- src/main/java/com/jayway/maven/plugins/android/common/FileNameHelper.java
+++ src/main/java/com/jayway/maven/plugins/android/common/FileNameHelper.java
@@ -1,7 +1,5 @@
 package com.jayway.maven.plugins.android.common;
 
-import org.codehaus.plexus.util.SelectorUtils;
-
 /**
  * FileNameHelper can make a valid filename.
  *
@@ -18,20 +16,4 @@ public class FileNameHelper
         return fileName.replaceAll( ILLEGAL_CHARACTERS_REGEX, SEPERATOR );
     }
 
-	public static boolean isMetaInfMatch( String name, String[] patterns )
-	{
-		if( patterns == null ) {
-			return false;
-		}
-
-		for( String inc : patterns )
-		{
-			if( SelectorUtils.matchPath( "META-INF/" + inc, name ) )
-			{
-				return true;
-			}
-		}
-
-		return false;
-	}
 }
