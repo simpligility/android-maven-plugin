89e7cdacc8fca2d7213b75034a51b503ee44d36c
pa314159 <pa314159@gmail.com>
2013-11-18 10:20:00 +0200

    issue-174: created helper method for checking META-INF matches

--- src/main/java/com/jayway/maven/plugins/android/common/FileNameHelper.java
+++ src/main/java/com/jayway/maven/plugins/android/common/FileNameHelper.java
@@ -1,5 +1,7 @@
 package com.jayway.maven.plugins.android.common;
 
+import org.codehaus.plexus.util.SelectorUtils;
+
 /**
  * FileNameHelper can make a valid filename.
  *
@@ -16,4 +18,20 @@ public class FileNameHelper
         return fileName.replaceAll( ILLEGAL_CHARACTERS_REGEX, SEPERATOR );
     }
 
+	public static boolean isMetaInfMatch( String name, String[] patterns )
+	{
+		if( patterns == null ) {
+			return false;
+		}
+
+		for( String inc : patterns )
+		{
+			if( SelectorUtils.matchPath( "META-INF/" + inc, name ) )
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
