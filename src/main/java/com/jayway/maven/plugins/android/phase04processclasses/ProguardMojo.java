85dccfe72fd06e5323ddc36d201ce4bd830320f2
Pappy STĂNESCU <pappy.stanescu@gmail.com>
2013-11-22 01:41:19 +0200

    fixed checkstyle violations

--- src/main/java/com/jayway/maven/plugins/android/phase04processclasses/ProguardMojo.java
+++ src/main/java/com/jayway/maven/plugins/android/phase04processclasses/ProguardMojo.java
@@ -371,7 +371,7 @@ public class ProguardMojo extends AbstractAndroidMojo
         commands.add( "-printusage" );
         commands.add( "'" + proguardDir + File.separator + "usage.txt'" );
         
-        File mapFile = new File(proguardDir, "mapping.txt");
+        File mapFile = new File( proguardDir, "mapping.txt" );
 
         commands.add( "-printmapping" );
         commands.add( "'" + mapFile + "mapping.txt'" );
@@ -391,7 +391,7 @@ public class ProguardMojo extends AbstractAndroidMojo
             throw new MojoExecutionException( "", e );
         }
         
-        if( parsedAttachMap )
+        if ( parsedAttachMap )
         {
             projectHelper.attachArtifact( project, "map", mapFile );
         }
