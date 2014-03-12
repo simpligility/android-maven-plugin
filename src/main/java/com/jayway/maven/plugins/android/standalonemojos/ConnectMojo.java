366d28da1d1f95d0bada651f29e3b8202f75ad89
Emmanuel DEMEY <emmanuel.demey@atos.net>
2014-03-12 15:38:27 +0100

    Add Connect and Disconnect mojos

---
+++ src/main/java/com/jayway/maven/plugins/android/standalonemojos/ConnectMojo.java
@@ -0,0 +1,54 @@
+package com.jayway.maven.plugins.android.standalonemojos;
+
+import com.jayway.maven.plugins.android.AbstractAndroidMojo;
+import com.jayway.maven.plugins.android.CommandExecutor;
+import com.jayway.maven.plugins.android.ExecutionException;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Connect external IP addresses to the ADB server.
+ *
+ * @author demey.emmanuel@gmail.com
+ * @goal connect
+ * @requiresProject false
+ */
+public class ConnectMojo extends AbstractAndroidMojo
+{
+
+    @Override
+    public void execute() throws MojoExecutionException, MojoFailureException
+    {
+
+        if ( ips.length > 0 )
+        {
+            CommandExecutor executor = CommandExecutor.Factory.createDefaultCommmandExecutor();
+
+            for ( String ip : ips )
+            {
+                getLog().debug( "Connecting " + ip );
+
+                String command = getAndroidSdk().getAdbPath();
+                List<String> parameters = new ArrayList<String>();
+                parameters.add( "connect" );
+                parameters.add( ip );
+
+                try
+                {
+                    executor.executeCommand( command, parameters );
+
+                }
+                catch ( ExecutionException e )
+                {
+                    throw new MojoExecutionException( String.format( "Can not connect %s", ip ), e );
+                }
+
+            }
+
+        }
+
+    }
+}
