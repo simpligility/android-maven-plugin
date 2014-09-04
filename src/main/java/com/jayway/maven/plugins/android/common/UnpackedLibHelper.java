77b0ab37be3875057fdd822c4ea054c02297393e
Manfred Moser <manfred@simpligility.com>
2014-09-04 10:39:52 -0700

    fix for https://github.com/jayway/maven-android-plugin/issues/443

--- src/main/java/com/jayway/maven/plugins/android/common/UnpackedLibHelper.java
+++ src/main/java/com/jayway/maven/plugins/android/common/UnpackedLibHelper.java
@@ -45,7 +45,7 @@ public final class UnpackedLibHelper
     public UnpackedLibHelper( ArtifactResolverHelper artifactResolverHelper, MavenProject project, Logger log )
     {
         this.artifactResolverHelper = artifactResolverHelper;
-        final File targetFolder = new File( project.getBasedir(), project.getBuild().getDirectory() );
+        final File targetFolder = new File( project.getBuild().getDirectory() );
         this.unpackedLibsDirectory = new File( targetFolder, "unpacked-libs" );
         this.log = log;
     }
