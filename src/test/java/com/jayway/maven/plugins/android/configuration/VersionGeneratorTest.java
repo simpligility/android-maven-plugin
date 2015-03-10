20585853304599f1be676a8dcb4a8cb536cfad54
Wang Xuerui <idontknw.wang@gmail.com>
2015-03-10 17:07:23 +0800

    VersionGeneratorTest: update tests

--- src/test/java/com/jayway/maven/plugins/android/configuration/VersionGeneratorTest.java
+++ src/test/java/com/jayway/maven/plugins/android/configuration/VersionGeneratorTest.java
@@ -26,9 +26,16 @@ public class VersionGeneratorTest
     }
 
     @Test
+    public void generateRegex() throws MojoExecutionException
+    {
+        assertEquals( 2147483647, new VersionGenerator( "1,2,3,4", "^(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)$" ).generate( "2.14.748.3647" ) );
+        assertEquals( 2147483647, new VersionGenerator( "4,3,2,1", "^(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)$" ).generate( "2147.483.64.7" ) );
+    }
+
+    @Test
     public void compare() throws MojoExecutionException {
         VersionGenerator gen = new VersionGenerator( "3,3,3" );
-    
+
         assertTrue( gen.generate( "1.0" ) < gen.generate( "1.0.1" ) );
         assertTrue( gen.generate( "2.0" ) > gen.generate( "1.0.1" ) );
    }
@@ -37,10 +44,10 @@ public class VersionGeneratorTest
     public void realCase() throws MojoExecutionException
     {
         VersionGenerator gen = new VersionGenerator( "1,1,2,1,4" );
-        
+
         int v1 = gen.generate( "4.1.16.8-SNAPSHOT.1946" );
         int v2 = gen.generate( "4.1.17.8-SNAPSHOT.1246" );
-        
+
         assertEquals(411681946, v1 );
         assertEquals(411781246, v2 );
 
@@ -49,14 +56,54 @@ public class VersionGeneratorTest
     }
 
     @Test
+    public void realCaseRegex() throws MojoExecutionException
+    {
+        {
+            VersionGenerator gen = new VersionGenerator( "1,1,2,1,4", "^(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)(?:-[A-Z]+\\.(\\d+))?$" );
+
+            int v1 = gen.generate( "4.1.16.8-SNAPSHOT.1946" );
+            int v2 = gen.generate( "4.1.17.8" );
+
+            assertEquals( 411681946, v1 );
+            assertEquals( 411780000, v2 );
+
+            assertTrue( v1 < v2 );
+        }
+
+        {
+            VersionGenerator gen = new VersionGenerator( "4,3,3", "^(\\d+)\\.(\\d+)(?:-(\\d+)-g[0-9a-f]+(?:-dirty)?)?$" );
+
+            int v1 = gen.generate( "0.0-38-g493f883" );
+            int v2 = gen.generate( "0.1" );
+
+            assertEquals( 38, v1 );
+            assertEquals( 1000, v2 );
+
+            assertTrue( v1 < v2 );
+        }
+
+        {
+            VersionGenerator gen = new VersionGenerator( "3,2,2,3", "^(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)" );
+
+            int v1 = gen.generate( "6.1.0.66-r1062275" );
+            int v2 = gen.generate( "6.2.0.0-r1234567" );
+
+            assertEquals( 60100066, v1 );
+            assertEquals( 60200000, v2 );
+
+            assertTrue( v1 < v2 );
+        }
+    }
+
+    @Test
     public void mixedCase() throws MojoExecutionException
     {
         VersionGenerator g1 = new VersionGenerator( "1,1,2,1,4" );
         VersionGenerator g2 = new VersionGenerator( "1,1,2,5" );
-        
+
         int v1 = g1.generate( "1.2.15.8-SNAPSHOT.1946" );
         int v2 = g2.generate( "2.1.6-SNAPSHOT.1246" );
-        
+
         assertEquals(121581946, v1 );
         assertEquals(210601246, v2 );
 
