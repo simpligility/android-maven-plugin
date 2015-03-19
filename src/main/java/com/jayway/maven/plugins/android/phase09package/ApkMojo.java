c09b5e468becedd9d450e7e190aec15d9c9dd039
Manfred Moser <manfred@simpligility.com>
2015-03-19 13:49:23 -0700

    remove unused import to comply to enforced style

--- src/main/java/com/jayway/maven/plugins/android/phase09package/ApkMojo.java
+++ src/main/java/com/jayway/maven/plugins/android/phase09package/ApkMojo.java
@@ -50,7 +50,6 @@ import java.io.File;
 import java.io.FileFilter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileInputStream;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.io.InputStream;
